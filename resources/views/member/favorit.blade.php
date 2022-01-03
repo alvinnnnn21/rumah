143788bcad01e2aad46f504f2f33efd639df6b49
alvinnnnn21 <33631963+alvinnnnn21@users.noreply.github.com>
2022-01-03 08:00:12 -0800

    fifth 3rd update

--- resources/views/member/favorit.blade.php
+++ resources/views/member/favorit.blade.php
@@ -48,22 +48,19 @@
             </div>
         </div>
         <div class="row mt-2">
-            <div class="col-md-12 px-5">
+            <div class="col-md-12 px-5 d-flex align-items-center flex-column">
                 @foreach($rumah as $r)
-                    <div class="row bg-light mb-3" style="border-radius: 10px; height: 60vh;">
+                    <div class="row bg-light mb-3 w-75 card-rumah" style="border-radius: 10px; height: 50vh; border: 2px solid #e2dfdf;">
                         <div class="col-md-4 img-card p-0">
-                            <img class="img-rumah-{{ $r->idrumah }}" src="{{ asset((count($r->gambar) > 0) ? "storage/images/rumah/" . $r->gambar[0]->gambar : "storage/images/rumah/no_image.png") }}">
+                            <img class="img-rumah-{{ $r->idrumah }}" src="{{ asset((count($r->gambar) > 0) ? "images/rumah/" . $r->gambar[0]->gambar : "images/rumah/no_image.png") }}">
                         </div>
                         <div class="col-md-8 pt-3 d-flex flex-column justify-content-between pb-3">
-                            <h4 style="font-weight: 700;">Rp {{ number_format($r->harga, 2, ',', '.') }}</h4>
-                            <a href="{{ url("/rumah") . "/" . $r->idrumah }}">
-                                <h5 class="mt-3">{{ $r->judul }}</h5>
-                            </a>
-                            <h6 class="text-secondary">
-                                <i class="fas fa-map-marker-alt mr-2 mt-3 text-danger"></i>
+                            <h4 class="my-0" style="font-weight: 700;">Rp {{ number_format($r->harga, 2, ',', '.') }}</h4>
+                            <h6 class="text-secondary my-0">
+                                <i class="fas fa-map-marker-alt mr-2 text-danger"></i>
                                 {{ $r->alamat }}
                             </h6>
-                            <div class="mt-4 d-flex flex-row" style="font-size: 20px;">
+                            <div class="d-flex flex-row" style="font-size: 20px;">
                                 <span>
                                     <i class="fas fa-bed"></i>
                                     {{ $r->jumlah_kamar }}
@@ -80,12 +77,16 @@
                                     <p class="text-secondary">Bangunan</p>
                                 </span>
                             </div>
-                            <p class="mt-3">{{ $r->keterangan }}</p>
-                            <h6 class="mb-3">Gambar Lain</h6>
-                            <div class="d-flex flex-row mb-1 img-other" style="overflow: auto;">
-                                @foreach($r->gambar as $key => $g)
-                                    <img data-id="{{ $r->idrumah }}" class="mr-2 {{ ($key == 0) ? "img-active" : "" }} img-carousel" style="width: 100px; height: 100px; border: 2px solid rgba(0, 0, 0, 0.2); border-radius: 10px;" src="storage/images/rumah/{{ $g->gambar }}">
-                                @endforeach
+                            @if(count($r->gambar) > 1)
+                                <h6>Gambar Lain</h6>
+                                <div class="d-flex flex-row mb-1 img-other" style="overflow: auto;">
+                                    @foreach($r->gambar as $key => $g)
+                                        <img data-id="{{ $r->idrumah }}" class="mr-2 {{ ($key == 0) ? "img-active" : "" }} img-carousel" style="width: 100px; height: 100px; border: 2px solid rgba(0, 0, 0, 0.2); border-radius: 10px;" src="images/rumah/{{ $g->gambar }}">
+                                    @endforeach
+                                </div>  
+                            @endif
+                            <div class="text-right">
+                                <a class="btn btn-primary" href='{{ url("/rumah") . "/" . $r->idrumah }}'>Lihat Detail</a>
                             </div>
                         </div>
                     </div>
